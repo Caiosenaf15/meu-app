461b0cb3d7b386e2ea2ce58a986b9772f5f7aecd
CaioSenaf15 <caiofreitas15@gmail.com>
2025-08-14 10:22:42 -0300

    clean

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
         render template, record if condition
       
     end
+
+    def sub_masked_email(email)
+        email.gsub(/(?<=.{2}).*@.*(?=\S{2})/, '****@****')
+    end
+    
+    def month_day_year(datetime)
+        datetime.strftime("%B %e, %Y")
+    end
 end
